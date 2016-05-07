8384fc6650bd440afa6c444960f7c9903eb5fc7b
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-07 17:26:08 -0400

    added event handler to tasks.js file to listen to the ajax event

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on("ready", ready);
+
+	function ready() {
+		$('new_task').on('ajax:success', newTask);
+
+		function newTask() {
+			window.location.reload();
+		}
+	}
+
+}
