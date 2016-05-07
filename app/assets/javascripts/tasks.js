6fd6641910ad7573616650e0001da21769382d01
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-07 17:53:53 -0400

    got to the end but still not working

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,12 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).on("ready", ready);
+$(document).on("ready page:load", ready);
 
 function ready() {
 	$('new_task').on('ajax:success', newTask);
 
-	function newTask() {
-		console.log(arguments);
-		window.location.reload();
+	function newTask(event, data) {
+		$('#tasks').append(data);
+		$('#task_name').val('');
 	}
 }
