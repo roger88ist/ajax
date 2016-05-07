f89623cc2571bce4dd48ff7bb254dee6dcfa7bbd
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-07 17:31:21 -0400

    turned off layout

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,12 +2,11 @@
 // All this logic will automatically be available in application.js.
 $(document).on("ready", ready);
 
-	function ready() {
-		$('new_task').on('ajax:success', newTask);
+function ready() {
+	$('new_task').on('ajax:success', newTask);
 
-		function newTask() {
-			window.location.reload();
-		}
+	function newTask() {
+		console.log(arguments);
+		window.location.reload();
 	}
-
 }
